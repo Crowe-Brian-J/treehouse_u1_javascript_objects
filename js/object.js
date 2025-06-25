be1e466248f62d752431c54a382ee538c45fbf21
Brian Crowe <brian.j.crowe@protonmail.com>
2025-06-25 14:52:29 -0400

    access object key value pairs in console

--- js/object.js
+++ js/object.js
@@ -1,3 +1,8 @@
+/*
+  arrays use index value, where objects use dot notation
+  -instead of person[0] to access name, you would use person.name to access Edward
+*/
+
 const person = {
   name: 'Edward',
   city: 'New York',
@@ -5,3 +10,6 @@ const person = {
   isStudent: true,
   skills: ['JavaScript', 'HTML', 'CSS']
 }
+
+const message = `Hi, I'm ${person.name}. I live in ${person.city}.`
+console.log(message)

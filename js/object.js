fddf61b2541d50d58607a4628750bad2c3217173
Brian Crowe <brian.j.crowe@protonmail.com>
2025-06-25 15:00:19 -0400

    ways to access and change object properties

--- js/object.js
+++ js/object.js
@@ -1,6 +1,9 @@
 /*
   arrays use index value, where objects use dot notation
   -instead of person[0] to access name, you would use person.name to access Edward
+
+  Can change or add key value pairs using dot notation =
+  -person.city = 'Boston'; (because it's better) would change city from 'New York' to Boston
 */
 
 const person = {
@@ -11,5 +14,10 @@ const person = {
   skills: ['JavaScript', 'HTML', 'CSS']
 }
 
-const message = `Hi, I'm ${person.name}. I live in ${person.city}.`
+person.nickname = 'Duke'
+const message = `Hi, I'm ${person.name}. I live in ${
+  person.city
+}. Most know me as ${person.nickname}. My age is ${person.age + 1}. I have ${
+  person.skills.length
+} skills: ${person.skills.join(', ')}.`
 console.log(message)
